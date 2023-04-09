0a5a5548c921ae2beb4050fd1eb3bc952bd5017d
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-04-09 18:18:29 -0300

    up

--- src/userCRUD/verificarEmailUser.php
+++ src/userCRUD/verificarEmailUser.php
@@ -1,10 +1,9 @@
 <?php 
-    $email = $_GET['email'];
-    $user = $_GET['user'];
+    $email = $_POST['email'];
+    $user = $_POST['user'];
     $fp = fopen('usuarios.csv','r');
     while( ($linha = fgetcsv($fp)) !== false){
         if($linha[2] == $email || $linha[0] == $user){
-            http_response_code(200);
             echo 'jaCadastrado';
             return;
         }
