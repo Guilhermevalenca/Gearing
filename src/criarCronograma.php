8e2374e82f0964377dbb80f8dc1ebe4d678c1071
Alice Manguinho <guilherm3vrp@gmail.com>
2023-04-10 11:29:05 -0300

    up

--- src/criarCronograma.php
+++ src/criarCronograma.php
@@ -40,8 +40,11 @@
             <input type="checkbox" name="materia[]" value='geografia'>
             geografia
         </label>
-        <input type="hidden" name="user" value="<?= $_SESSION['username'] ?>">
-        <input type="submit">
+        <br>
+        <label>
+            <input type="hidden" name="user" value="<?= $_SESSION['username'] ?>">
+            <input type="submit">
+        </label>
     </form>
     <button onclick="window.location.href='./menu.php'">Voltar para o menu</button>
     <?php if($_SESSION['mensagem'] != ''): ?>
