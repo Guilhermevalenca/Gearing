07d1387db8369da535e0c0fac6bbdfe406ea2f21
Claudiane Rodrigues <113156051+Cl4udiAnn3@users.noreply.github.com>
2023-04-16 00:23:26 -0300

    up

--- script/cronograma/criarCronograma.js
+++ script/cronograma/criarCronograma.js
@@ -1,11 +1,20 @@
- //script para verificar se todas as perguntas tiveram ao menos uma resposta
+//script para verificar se todas as perguntas tiveram ao menos uma resposta
+
         var form = document.getElementById("form");
 
         form.addEventListener('submit', (event) => {
             event.preventDefault();
+
+           
+            var horarios = form.elements["horario[]"];
+            var materias = form.elements["materia[]"];
+             var dias form.elements["dias[]"];
+            //var tempoEstudar = form.elements["tempo[]"];
+
             var horarios = form.elements["horario[]"];
             var materias = form.elements["materia[]"];
             var tempoEstudar = form.elements["tempo[]"];
+
             function teste(parametro){
                 for (let i = 0; i < parametro.length; i++) {
                     if(parametro[i].checked) {
@@ -17,6 +26,10 @@
                 alert("Por favor, escolha uma disponibilidade");
             }else if(!teste(materias)){
                 alert("Por favor, escolha uma materia");
+            //}else if(!teste(tempoEstudar)){
+              //  alert("Por favor, escolha um horario");
+            }elseif(!teste(dias)){
+                alert("Você não selecionou nenhum dia!");
             }else if(!teste(tempoEstudar)){
                 alert("Por favor, escolha um horario");
             }else{
