904221cca443460df77362e46bdcca87fda6a8e7
Cl4udiAnn3 <cra@discente.ifpe.edu.br>
2023-04-16 15:44:07 -0300

    up

--- src/userCRUD/login.php
+++ src/userCRUD/login.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <link rel= "stylesheet" type="text/css" href= "../css/login.css/" />
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gearing</title>
+    <link rel="stylesheet" href="/css/login.css">
 </head>
 <body>
     <form id="form" method="POST" action="/php/userCRUD/verificarExistenciaUser.php">
