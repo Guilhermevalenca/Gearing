aebac1caccd2e540ca91365529decfa08fc339d7
guilhermevalenca <guilherm3vrp@gmail.com>
2023-04-18 19:53:16 -0300

    up

--- src/userCRUD/login.php
+++ src/userCRUD/login.php
@@ -15,7 +15,7 @@
         <img class="logo_sessao_principal" src="/logos/LET'S DANCE.gif">
         <div class="formulario_sessao_principal">
             <form id="form" method="POST" action="/php/userCRUD/verificarExistenciaUser.php">
-                <input id="login" type="text" name="login" placeholder="Digite seu email" required><br>
+                <input id="login" type="email" name="login" placeholder="Digite seu email" required><br>
                 <input id="senha" type="password" name="senha" placeholder="Digite sua senha" required><br>
                 <button type="submit">Entrar</button>
                 <button onclick="window.location.href = '/' ">Voltar</button>
