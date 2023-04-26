2550dc343d6c24757ab606f4e5c99a3ff33c23a0
Mauro Teixeira <alicemanguinho999@gmail.com>
2023-04-26 14:17:30 -0300

    metodolgias

--- src/metodologias/metodologias.php
+++ src/metodologias/metodologias.php
@@ -13,6 +13,7 @@
     <h1> Questionário de Metodologias de Aprendizagem </h1>
     <button onclick="window.location.href='/src/menu.php'">Voltar para o menu</button>
     <form id="form" method="GET" action="./suaMetodologia.php">
+
         <h2>Enquanto estou estudando gosto de:</h2>
         <label>
            <input type="checkbox" name="aprender[]" value="sentimentos">
@@ -31,11 +32,11 @@
         <br>
         <label>
             <input type="checkbox" name="aprender[]" value="observar">
-            Observar e ouvir 
+            Observar e ouvir
         </label>
         <br>
 
-        <h2> Tenho facilidade em aprender quando: </h2>
+        <h2> Costumo ter facilidade em aprender, principalmente quando: </h2>
         <label>
             <input type="checkbox" name="metodos[]" value="ouvir">
             Ouço e observo com atenção
@@ -43,12 +44,12 @@
         <br>
         <label>
             <input type="checkbox" name="metodos[]" value="logica">
-            Utlizando pensamento lógico
+            Utlizo de raciocínio lógico
         </label>
         <br>
         <label>
             <input type="checkbox" name="metodos[]" value="palpites">
-            Confio em meus palpites e impressões
+            Me baseio em meus palpites e impressões
         </label>
         <br>
         <label>
@@ -60,17 +61,17 @@
         <h2> Quando estou aprendendo costumo: </h2>
         <label>
             <input type="checkbox" name="estudos[]" value="explicacao">
-            Buscar explicação para os assuntos
+            Buscar explicações para os tópicos estudados
         </label>
         <br>
         <label>
             <input type="checkbox" name="estudos[]" value="responsavel">
-            Ser responsável acerca das coisas 
+            Ser responsável acerca das coisas (Disciplinado)
         </label>
         <br>
         <label>
             <input type="checkbox" name="estudos[]" value="concentrar">
-            Ficar quieto e concentrado
+            Ficar quieto(a) e me concentrar 
         </label>
         <br>
         <label>
@@ -78,78 +79,205 @@
             Ter sentimentos e reações fortes
         </label>
         <br>
-        <label>
-            <input type="checkbox" name="estudos[]" value="explicacao">
-            explicações
-        </label>
-        <h2> Aprendo quando: </h2>
+       
+        <h2> Aprendo: </h2>
         <label>
             <input type="checkbox" name="estudar[]" value="sinto">
-            Sinto
+            Sentindo (Tristeza, raiva, admiração, curiosidade, etc.)
         </label>
         <br>
         <label>
             <input type="checkbox" name="estudar[]" value="faco">
-            Faço
+            Fazendo
         </label>
         <br>
         <label>
             <input type="checkbox" name="estudar[]" value="observo">
-            Observo 
+            Observando
         </label>
         <br>
         <label>
-            <input type="checkbox" name="estudar[]" value="penso">
-            Penso
+            <input type="checkbox" name="[]" value="">
+            Pensando
         </label>
         <br>
         <label>
 
         <h2> Enquanto aprendo gosto de: </h2>
         <label>
-            <input type="checkbox" name="durante[]" value="experiencias">
+            <input type="checkbox" name="[]" value="">
             Me abrir a novas experiências
         </label>
         <br>
         <label>
-            <input type="checkbox" name="durante[]" value="examino">
+            <input type="checkbox" name="[]" value="">
             Examinar todos os ângulos da questão
         </label>
         <br>
         <label>
-            <input type="checkbox" name="durante[]" value="analisar">
-            Analisar as coisas, desdobrá-las em partes
+            <input type="checkbox" name="[]" value="">
+            Analisar as coisas (Desdobrá-las em partes para entendê-las/resolvê-las)
         </label>
         <br>
         <label>
-            <input type="checkbox" name="durante[]" value="testar">
-            Testar as coisas
+            <input type="checkbox" name="[]" value="">
+            Testar as coisas (Métodos, práticas, etc.)
         </label>
         <br>
 
         <h2> Enquanto estudo sou: </h2>
         <label>
-            <input type="checkbox" name="durante[]" value="testar">
+            <input type="checkbox" name="[]" value="">
             Alguém que prefere observar
         </label>
         <br>
         <label>
-            <input type="checkbox" name="durante[]" value="testar">
-            Ativo(a)
+            <input type="checkbox" name="[]" value="">
+            Ativo(a) (Utilizo mais de um método de estudo)
         </label>
         <br>
         <label>
-            <input type="checkbox" name="durante[]" value="testar">
-            Intuitivo(a)
+            <input type="checkbox" name="[]" value="">
+            Intuitivo(a) (Entender quais são suas dificuldades e conseguir identificar o que se encaixa melhor para seu processo de aprendizagem)
         </label>
         <br>
         <label>
-            <input type="checkbox" name="durante[]" value="testar">
-            Lógico(a)
+            <input type="checkbox" name="[]" value="">
+            Lógico(a) (Utilizar a lógica e a razão para entender os temas abordados)
         </label>
         <br>
         
         <h2> Aprendo melhor atrvés de: </h2>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Observação 
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Interações pessoais (Adquirir conheço por meio de interações com outros individuos)
+        </label>
+        <br>
+        <label>
+           <input type="checkbox" name="[]" value="">
+           Teorias racionais (Modelos conceituais baseados em princípios lógicos e raciocínio dedutivo)
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Oportunidades para experimentar e praticar
+        </label>
+        <br>
+
+        <h2>Enquanto aprendo:</h2>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Gosto de ver o resultado do meu trabalho
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Me interesso em ter/conhecer ideias e teorias 
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Penso antes de agir (planejar-se)
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Sinto-me pessoalmente envolvido nos assuntos
+        </label>
+        <br>
+
+        <h2> Aprendo melhor quando me apoio em: </h2>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Minhas observações
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Minhas impressões
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Posso experimentar coisas por mim mesmo(a)
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Me apoio em minhas ideias(a)
+        </label>
+        <br>
+        
+        <h2> Quando estou aprendendo costumo ser: </h2>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Compenetrada
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Flexível
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Responsável 
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Racional
+        </label>
+        <br>
+
+        <h2> Enquanto aprendo costumo: </h2>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Me envolver totalmente
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Gostar de observar
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Avaliar as coisas 
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Estar ativo 
+        </label>
+        <br>
+
+        <h2> Aprendo melhor quando: </h2>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Analiso as ideias
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Sou receptivo e mente aberta
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Sou cuidadoso (Tomar medidas para garantir que a aprendizagem seja realizada de forma precisa, sem erros)
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="[]" value="">
+            Sou prático (Busco formas de tornar o aprendizado mais significativo e relevante, e aplicar o que está sendo estudado na prática)
+        </label>
+        <br>
 
 </body>
 </html>
