2db70089410af4dbe1db54a76f06613758cfff83
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-04-28 19:16:19 -0300

    up

--- src/cronograma/criarCronograma.php
+++ src/cronograma/criarCronograma.php
@@ -115,8 +115,8 @@
         <label id="gerarInputs">
             Outras materias:
             <input id="outrasMaterias" type="text" name="materia[]" placeholder="Digite aqui a matéria" >
-            <button type="text" onclick="novoInput()">Adicionar uma nova materia</button>
         </label>
+        <button type="text" onclick="novoInput()">Adicionar uma nova materia</button>
         <h2>Quanto tempo você pretende estudar para cada materia?</h2>
         <label>
             <input type="number" name="tempoMateria[]" min="15" max="60" step="5" required>
