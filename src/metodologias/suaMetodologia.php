cd7575ed34786e823a88d4be511315903784de63
Mauro Teixeira <alicemanguinho999@gmail.com>
2023-05-03 15:20:54 -0300

    barra do questionário

---
+++ src/metodologias/suaMetodologia.php
@@ -0,0 +1,6 @@
+<form action="/metodologias.php" method="GET">
+    <label for="input1">a questao
+</label><br>
+<input type="range" id="input1" name="inputRange" min='1' max="4">
+<input type="submit">
+</form>
