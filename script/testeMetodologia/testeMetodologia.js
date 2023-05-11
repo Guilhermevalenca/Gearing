141eef68a22ccb202a77a1a13c844c415d82bc2d
Guilhermevalenca <cra@discente.ifpe.edu.br>
2023-05-11 07:27:01 -0300

    brain explodes

--- script/testeMetodologia/testeMetodologia.js
+++ script/testeMetodologia/testeMetodologia.js
@@ -1,9 +1,5 @@
 const formulario = document.getElementById['form'];
 const perguntas = document.querySelectorAll['.pergunta'];
 perguntas.forEach(pergunta=>{
-
-})
-const respostas = document.querySelectorAll['.opcao'];
-respostas.forEach((resposta,letra)=>{
-
+    const respostas = pergunta.querySelectorAll('.opcao');
 })
