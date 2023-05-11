147ee91b1a6b6246d1a95e1ec5dd0df0e24c6d4b
Guilhermevalenca <cra@discente.ifpe.edu.br>
2023-05-11 07:38:51 -0300

    up

--- script/testeMetodologia/testeMetodologia.js
+++ script/testeMetodologia/testeMetodologia.js
@@ -1,5 +1,17 @@
+let EC = 0;
+let CA = 0;
+let OR = 0;
+let EA = 0;
 const formulario = document.getElementById['form'];
 const perguntas = document.querySelectorAll['.pergunta'];
 perguntas.forEach(pergunta=>{
     const respostas = pergunta.querySelectorAll('.opcao');
-})
+    respostas[0].value;
+    respostas[1].value;
+    respostas[2].value;
+    respostas[3].value;
+
+    
+});
+
+
