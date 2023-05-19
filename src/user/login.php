a8556864890110aa790cdf7624ab7487d057fd19
Claudiane Rodrigues <cra@discente.ifpe.edu.br>
2023-05-19 10:24:34 -0300

    up

--- src/user/login.php
+++ src/user/login.php
@@ -15,8 +15,14 @@
     <section class="sessao_principal">
         <div class="formulario_sessao_principal">
             <form id="form" method="POST" action="/php/user/authenticateUser.php">
-                <input id="login" type="email" name="login" placeholder="Digite seu email" required><br>
-                <input id="senha" type="password" name="senha" placeholder="Digite sua senha" required><br>
+                <label>
+                    E-mail: <br>
+                <input id="login" type="email" name="login" placeholder="Digite seu email" required>
+            </label><br>
+            <label>
+                Senha: <br>
+                <input id="senha" type="password" name="senha" placeholder="Digite sua senha" required>
+            </label><br>
                 <button type="submit">Entrar</button>
                 <button id="voltar" onclick="window.location.href = '/' ">Voltar</button>
             </form>
