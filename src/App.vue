47fc01ef8d6cca0b6e624d768711267baeb75e33
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-05-19 23:14:33 -0300

    aprendendo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <TheHeader 
+    v-if="showHeader"
+  />
+  <div 
+    v-if="accessLevel === 'admin'"
+  >
+      Admin
+  </div>
+  <div v-else-if="accessLevel === 'marketing'">Marketing</div>
+  <div v-else>User</div>
+  <img 
+    alt="Vue logo" 
+    src="./assets/logo.png"
+  >
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeader
+  },
+  data() {
+    return{
+      showHeader: true,
+      accessLevel: 'admin'
+    }
   }
 }
 </script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+
+        <img src="https://via.placeholder.com/150" v-bind:alt="imgAll">
+
+        <div 
+            v-for="(obj, index) in todo"
+            v-bind:key="obj.id"
+        >
+        {{ index }} - {{ obj.name }}
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'App',
+        data() {
+            return{
+                imgAll: 'foto de Guilherme',
+                todo: [
+                    {
+                        'id': 1,
+                        'name': 'Guilherme',
+                        'surName': 'Valença'
+                    },
+                    {
+                        'id': 2,
+                        'name': 'Claudiane',
+                        'surName': 'Valença'
+                    }
+                ]
+            }
+        }
+    }
+</script>
