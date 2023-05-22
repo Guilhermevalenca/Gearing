9e6f6bd0d68ac7624a219e457c384b1239f7f683
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-05-22 16:40:20 -0300

    up

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
+  <nav class="menu">
+    <router-link to="/">Home</router-link>
+    <div v-for="rota in rotas" :key="rota">
+      <router-link :to="rota">{{ rota }}</router-link>
+    </div>
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script>
+export default{
+  name: 'App',
+  data() {
+    return {
+      rotas: ['servicos','menu','helloWord','rapariga','essasCoisas']
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,9 +29,7 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
+
 
 nav a {
   font-weight: bold;
---
+++ src/views/ServicesView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Seus serviços
+    </div>
+</template>
