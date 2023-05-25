98953aad4f7494c6a587eb7c916a0506509b38f7
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-05-25 14:29:54 -0300

    aaaaain

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
-<?php 
-    session_start();
-    if(isset($_SESSION['username'])){
-        header('location: ./src/menu.php');
-        exit();
-    }else{
-        session_destroy();
-    }
+<?php
+session_start();
+if (isset($_SESSION['username'])) {
+    header('location: ./src/menu.php');
+    exit();
+} else {
+    session_destroy();
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,21 +18,26 @@
     <link rel="stylesheet" href="/css/index.css">
     <link rel="stylesheet" href="/css/global.css">
 </head>
+
 <body>
-    <img src="./css/archive/logo.gif" class="logo">
-    <h1 class="titulo">Gearing</h1>
-    <div class="apresentacao">
-        <ul>Bem Vindos!</ul>
-        <ul class="descricao">Olá, sou o Gearing! Estou aqui para te ajudar a encontrar um método de estudo que mais se aplique a você; vou te apresentar algumas formas de começar e/ou complementar seus estudos, sinta-se a vontade para compartilhar as sugestões de metodologias de aprendizagem que você conhece em nosso fórum, e assim, ajudar outros estudantes a montar seu próprio cronograma de estudos.
-        </ul>
-    
-    </div>
-    <div class="relacaoCRUD">
-        <button class="criarRealizarLogin" onclick="window.location.href = './src/user/login.php' ">Login</button>
-        <button class="criarRealizarLogin" onclick="window.location.href = './src/user/createUser.php' ">Criar Conta</button>
-    </div>
-        
+    <header class="header-home-page">
+        <img src="./css/archive/logo.gif" class="logo">
+        <h1 class="titulo">Gearing</h1>
+    </header>
+    <section class="main-content-home-page">
+        <div class="apresentacao">
+            <h1 class="bem-vindo">Bem Vindos!</h1>
+            <p class="descricao">Olá, sou o Gearing! Estou aqui para te ajudar a encontrar um método de estudo que mais se aplique a você; vou te apresentar algumas formas de começar e/ou complementar seus estudos, sinta-se a vontade para compartilhar as sugestões de metodologias de aprendizagem que você conhece em nosso fórum, e assim, ajudar outros estudantes a montar seu próprio cronograma de estudos.
+            </p>
+        </div>
+        <div class="relacaoCRUD">
+            <button class="criarRealizarLogin" onclick="window.location.href = './src/user/login.php' ">Login</button>
+            <button class="criarRealizarLogin" onclick="window.location.href = './src/user/createUser.php' ">Criar Conta</button>
+        </div>
+    </section>
+
 </body>
+
 </html>
 <!-- 
     texto a ser ignorado:
