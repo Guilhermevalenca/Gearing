8c1b59179e8ecdffa7462a94fe6f2a7b425d99db
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-05-31 19:20:06 -0300

    up

--- src/components/cronogramPage/CreateCronogram.vue
+++ src/components/cronogramPage/CreateCronogram.vue
@@ -2,63 +2,70 @@
 <div>
 <form @submit.prevent="">
     <label> 
-        <input v-model="matter" type="checkbox" value="matematica">
+        <input v-model="matters" type="checkbox" value="matematica">
         Matematica
     </label>
     <label>
-        <input v-model="matter" type="checkbox" value="fisica">
+        <input v-model="matters" type="checkbox" value="fisica">
         Fisica
     </label>
     <label>
-        <input v-model="matter" type="checkbox" value="quimica">
+        <input v-model="matters" type="checkbox" value="quimica">
         Quimica
     </label>
     <label>
-        <input v-model="matter" type="checkbox" value="biologia">
+        <input v-model="matters" type="checkbox" value="biologia">
         Biologia
     </label>
     <label>
-        <input v-model="matter" type="checkbox" value="historia">
+        <input v-model="matters" type="checkbox" value="historia">
         Historia
     </label>
     <label> 
-        <input v-model="matter" type="checkbox" value="portugues">
+        <input v-model="matters" type="checkbox" value="portugues">
         Portugues
     </label>
     <label> 
-        <input v-model="matter" type="checkbox" value="geografia">
+        <input v-model="matters" type="checkbox" value="geografia">
         Geografia
     </label>
     <label> 
-        <input v-model="matter" type="checkbox" value="filosofia">
+        <input v-model="matters" type="checkbox" value="filosofia">
         Filosofia
     </label>
     <label> 
-        <input v-model="matter" type="checkbox" value="sociologia">
+        <input v-model="matters" type="checkbox" value="sociologia">
         Sociologia
     </label>
     <label> 
-        <input v-model="matter" type="checkbox" value="artes">
+        <input v-model="matters" type="checkbox" value="artes">
         Artes
     </label>
     <label> 
-        <input v-model="matter" type="checkbox" value="Ingles">
+        <input v-model="matters" type="checkbox" value="Ingles">
         Ingles
     </label>
     <label> 
-        <input v-model="matter" type="checkbox" value="espanhol">
+        <input v-model="matters" type="checkbox" value="espanhol">
         Espanhol
     </label>
     <div>
-        <button @click.prevent="addOtherMatters()">adicionar materia</button>
-        <button @click.prevent="removeOtherMatters()">remover materia</button>
-    </div>
-    <div v-for="otherMatter in otherMatters" :key="otherMatter">
-        
+        Outras materias:
+        <input type="text" v-model="otherMatter" placeholder="nova materia">
+        <button @click.prevent="addOtherMatter()">Adicionar materias</button>
     </div>
     <input type="submit">
 </form>
-    {{ matter }} 
+<div>
+    Materias adicionadas:
+    <div class="list" v-if="this.matters.length != 0">
+        <div class="list-subjects" v-for="(matter, index) in matters" :key="index">
+           <div class="list-subjects-individually">
+                <p>{{ matter }}</p><button>remover</button>
+           </div>
+        </div>
+    </div>
+</div>
 </div>
 </template>
 <script>
@@ -67,17 +74,27 @@ export default{
     components: {} ,
     data() {
         return {
-            matter: [],
-            otherMatters: []
+            matters: [],
+            otherMatter: ''
         }
     },
     methods: {
-        addOtherMatters() {
-            this.otherMatters.push({});
-        },
-        removeOtherMatters() {
-            
+        addOtherMatter() {
+            this.matters.push(this.otherMatter),
+            this.otherMatter = ''
         }
     }
 }
 </script>
+<style scoped>
+.list{
+    display: grid;
+    border-style: solid;
+    width: 25%;
+    justify-content: center;
+}
+.list-subjects-individually{
+    display: flex;
+    justify-content: center;
+}
+</style>
