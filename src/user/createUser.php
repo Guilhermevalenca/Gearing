74ebf3470bb63116ba3cc76a9ee2f501feebf366
RodriguesClaudiane <113156051+RodriguesClaudiane@users.noreply.github.com>
2023-06-01 10:01:36 -0300

    eu ajeitei aqui eu

--- src/user/createUser.php
+++ src/user/createUser.php
@@ -12,26 +12,28 @@
     <section class="sessao_principal"> 
         <div class="formulario_sessao_principal">
             <form id="formulario" method="POST" action="/php/user/addUser.php">
-                <label class="test">usuario:
+                <label class="test">Usuário:
                     <input type="text" id="user" name="user" placeholder="Digite aqui seu nome de usuario" required>
                 </label><br>
-                <label>nome:
+                <label>Nome:
                     <input type="text" name="nome" placeholder="Digite aqui seu nome" required>
                 </label><br>
                 <label>
-                <label>email:
+                <label>Email:
                     <input type="email" id="email" name="email" placeholder="Digite aqui seu email" required>
                 </label><br>
-                <label>senha:
+                <label>Senha:
                     <input type="password" id="senha" name="senha" placeholder="Digite aqui sua senha" minlength="8" required>
                 </label><br>
                 <label>Confirme sua senha:
                     <input type="password" id="confirme" name="confirmaSenha" placeholder="Digite novamente sua senha" required>
                 </label><br>
+                <div id="butao">
                 <button type="submit">Entrar</button>
                 <button id="voltar" onclick="window.location.href = '/' ">Voltar</button>
-                <!-- Pergunta: Qual método você costuma usar para estudar? -->
-        </div>
+             </div>
+             </div>
+         
         </form>
     </section>
     <script defer src='/script/user/checkingNewUser.js'></script>
