6e8fe854519906855c5c1d0f5b9c8fb808284c67
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-06-02 20:37:24 -0300

    ESSA POHA NÂO QUER FUNCIONAR KARALHOOOOOOOOO

--- src/components/SchedulePage/MountedSchedule.vue
+++ src/components/SchedulePage/MountedSchedule.vue
@@ -5,37 +5,41 @@
 </div>
 
 <div class="schedule">
-    <div class="schedule-matters" ref="sortableList">
-        <ul v-for="(subject, index) in matters" :key="index">
-            {{ subject }}
-        </ul>
-    </div>
-    <div class="schedule-empty">
-        <table>
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Domingo</th>
-                    <th>Segunda</th>
-                    <th>Terça</th>
-                    <th>Quarta</th>
-                    <th>Quinta</th>
-                    <th>Sexta</th>
-                    <th>Sabado</th>
-                </tr>
-            </thead>
-            <tbody>
-                <div v-for="(hour, row) in turns" :key="row">
-                    <tr>
-                        <td>{{ hour }}</td>
-                        <div v-for="(receiving, column) in receivingSubjects" :key="column">
-                            <td></td>
-                        </div>
-                    </tr>
-                </div>
-            </tbody>
-        </table>
-    </div>
+
+<div class="schedule-matters" ref="sortableSubject">
+    <ul v-for="(subject, index) in matters" :key="index">
+        {{ subject }}
+    </ul>
+</div>
+
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>Domingo</th>
+            <th>Segunda</th>
+            <th>Terça</th>
+            <th>Quarta</th>
+            <th>Quinta</th>
+            <th>Sexta</th>
+            <th>Sabado</th>
+        </tr>
+    </thead>
+    <tbody>
+        <div ref="sortableReceiving">
+        <tr v-for="(hour, index) in turns" :key="index">
+            <td>{{ hour }}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        </div>
+    </tbody>
+</table>
 </div>
 
 </template>
@@ -57,19 +61,29 @@ export default{
         }
     },
     mounted() {
-        new Sortable(this.$refs.sortableList, {
+        new Sortable(this.$refs.sortableSubject, {
+            group: 'shared',
             onEnd: this.handleSortEnd
+        });
+        this.turns.forEach((element, index) => {
+            new Sortable(this.$refs[`sortableReceiving${index}`], {
+                group: 'shared',
+                onEnd: this.handleSortEnd,
+            });
         });
         this.receivingSubjects.length = 7;
     },
     methods: {
         handleSortEnd(event) {
-        // Obtenha os índices iniciais e finais do arrastar
-        const { oldIndex, newIndex } = event;
-
-        // Atualize a ordem dos itens no array "items"
-        const movedItem = this.matters.splice(oldIndex, 1)[0];
-        this.matters.splice(newIndex, 0, movedItem);
+          const { oldIndex, newIndex, from, to } = event;
+            
+          // Verifica se o item foi movido de uma div para outra
+            if (from !== to) {
+                const movedItem = from === this.$refs.sortableSubject ? this.matters : this.receivingSubjects;
+                const targetItems = to === this.$refs.sortableSubject ? this.matters : this.receivingSubjects;
+                const item = movedItem.splice(oldIndex, 1)[0];
+                targetItems.splice(newIndex, 0, item);
+            }
         }
     },
     watch: {
@@ -101,10 +115,10 @@ export default{
 </script>
 
 <style scoped>
-    .schedule-matters{
-        display: flex;
-    }
-    .schedule-matters, .schedule-empty{
-        justify-content: center;
-    }
+.schedule-matters{
+    display: flex;
+}
+.schedule-matters{
+    justify-content: center;
+}
 </style>
