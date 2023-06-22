bf7918d88ec3e7b54db3a28b4433c30bbb7ca44c
assie <aross@discente.ifpe.edu.br>
2023-06-22 10:53:50 -0300

    methodoly att

---
+++ src/views/MethodologyPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+      <h1>{{ metodologia }}</h1>
+      <button @click="() => {methods.pomodoro = !methods.pomodoro}">Pomodoro</button>
+      <div v-if="methods.pomodoro">
+        faz tal coisa
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        metodologia: 'Bem vindo(a) às Metodologias!',
+        methods: {
+            pomodoro: false
+        }
+      };
+    },
+    methods: {
+      changeMessage() {
+        this.message = 'Nova mensagem!';
+      }
+    }
+  };
+  </script>
+  
