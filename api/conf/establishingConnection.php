5a09128b41668a9bc99dd38566caa6c9e5ebdd0c
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-07-24 14:51:16 -0300

    up

--- api/conf/establishingConnection.php
+++ api/conf/establishingConnection.php
@@ -1,11 +1,12 @@
 <?php
+$host = 'localhost';
 $port = "3306";
 $username = "gearing"; //Nome de usuario do seu mysql
 $password = "gearing123"; //senha do usuario do mysql
 $dbname = "GEARING"; //nome da sua DATABASE
 
 try {
-    $conn = new PDO("mysql:host=localhost:$port;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$host:$port;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Conexão bem-sucedida";
 } catch (PDOException $e) {
