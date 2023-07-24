a82bed52c107c752e35bfb1f5c24efdeca5746d1
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-07-24 15:20:59 -0300

    up

--- api/index.php
+++ api/index.php
@@ -1,31 +1,5 @@
 <?php
 require 'conf/init.php';
 
-/*
-$array = [
-    'gui' => [
-        'email' => 'gui@gmail.com',
-        'senha' => 'rock1109'
-    ],
-    'nix' => [
-        'email' => 'cra@discente.ifpe.edu.br',
-        'senha' => 'zeroum'
-    ],
-    'gearing' => [
-        'email' => 'gearing@gmail.com',
-        'senha' => 'gearing123'
-    ],
-    'assiria' => [
-        'email' => 'assie@gmail.com',
-        'senha' => '123'
-    ]
-];
 
-foreach($array as $row => $users){
-    echo 'row: ' . $row . "\n";
-    foreach($users as $cols => $data){
-        echo "$cols: " . hash('sha256',$data) . "\n";
-    }
-}
-*/
 echo "OK";
